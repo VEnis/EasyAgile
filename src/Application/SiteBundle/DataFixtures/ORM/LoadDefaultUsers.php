bb8fba7e612aeb4822b5a6116692958c6c555480
Vyacheslav Enis <venis@difane.com>
2012-12-25 06:15:32 +0200

    EAWEB-1 Fixed users fixtures. Now for each user corresponded invitation also created

--- src/Application/SiteBundle/DataFixtures/ORM/LoadDefaultUsers.php
+++ src/Application/SiteBundle/DataFixtures/ORM/LoadDefaultUsers.php
@@ -9,20 +9,63 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Application\Sonata\UserBundle\Entity\Invitation;
+use Application\Sonata\UserBundle\Entity\User;
 
 class LoadDefaultUsers implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
     private $container;
+
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
 
     public function load(ObjectManager $manager)
     {
-        $manipulator = $this->container->get('fos_user.util.user_manipulator');
-        $manipulator->create("VEnis", "MyDatacom_Passwd!", "venis@difane.com", true, true);
+        $this->manager = $manager;
+
+        $this->createUser("VEnis", "MyDatacom_Passwd!", "venis@difane.com", true, true);
+
+        $this->manager->flush();
     }
 
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
+    }
+
+    /**
+     * Creates user and invitation for it
+     * @param $username
+     * @param $password
+     * @param $email
+     * @param $active
+     * @param $superadmin
+     */
+    protected function createUser($username, $password, $email, $active, $superadmin)
+    {
+        // Creating invitation
+        $invitation = new Invitation();
+        $invitation->setEmail($email);
+        $invitation->send();
+        $this->manager->persist($invitation);
+
+        // Creating user
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPlainPassword($password);
+        $user->setEnabled((Boolean) $active);
+        $user->setSuperAdmin((Boolean) $superadmin);
+        $user->setInvitation($invitation);
+
+        $userManager->updateUser($user, false);
     }
 
 }
